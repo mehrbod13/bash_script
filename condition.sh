39c83a2a4d006e43f7bb85ab96925cc1d1bb89f2
mehrbod13 <tbhmahoon@gmail.com>
2025-08-11 00:51:52 +0330

    expressions and conditions

--- condition.sh
+++ condition.sh
@@ -2,9 +2,31 @@
 
 read -p "Search for file: " file
 
-if [[ -a ${file} ]]; then 
+# -z return true if string is empty
+if [[ -z ${file} ]]; then
+    echo "filename cant be empty"
+    exit 1
+fi
+
+# -r returns true if file is readable
+# -s return true if file size is greater than zero
+if [[ -r ${file} ]] && [[ -s ${file} ]]; then 
     echo "$file exists"
     cat $file
 else
     echo "$file dont exists"
+fi
+
+# arithmetic operators
+read -p "number 1: " num1
+read -p "number 2: " num2
+
+# -gt means grater than
+# -eq means equal
+if [[ $num1 -gt $num2 ]]; then 
+    echo "$num1 is bigger than $num2"
+elif [[ $num1 -eq $num2 ]]; then
+    echo "$num2 is equal to $num1"
+else
+    echo "$num2 is bigger than $num1"
 fi
